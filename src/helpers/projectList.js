e258c7cffe85b8bdfcc0f211ae98bd1b8531b488
SandraSor <soronastya@mail.ru>
2023-03-09 15:28:28 +0300

    Изменен проект sneakers

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -1,60 +1,60 @@
-import project01 from "./../img/projects/counter.jpg";
-import project01Big from "./../img/projects/counter-big.jpg";
+import project01 from './../img/projects/counter.jpg';
+import project01Big from './../img/projects/counter-big.jpg';
 
-import project02 from "./../img/projects/quiz.jpg";
-import project02Big from "./../img/projects/quiz-big.jpg";
+import project02 from './../img/projects/quiz.jpg';
+import project02Big from './../img/projects/quiz-big.jpg';
 
-import project03 from "./../img/projects/ul.jpg";
-import project03Big from "./../img/projects/ul-big.jpg";
+import project03 from './../img/projects/ul.jpg';
+import project03Big from './../img/projects/ul-big.jpg';
 
-import project04 from "./../img/projects/xo.jpg";
-import project04Big from "./../img/projects/xo-big.jpg";
+import project04 from './../img/projects/xo.jpg';
+import project04Big from './../img/projects/xo-big.jpg';
 
-import project05 from "./../img/projects/sneakers.jpg";
-import project05Big from "./../img/projects/sneakers.jpg";
+import project05 from './../img/projects/sneakers1.jpg';
+import project05Big from './../img/projects/sneakers-big.jpg';
 
 const projects = [
-  {
-    id: 1,
-    title: "Counter",
-    skills: "Skills: HTML, CSS, React",
-    img: project01,
-    imgBig: project01Big,
-    gitHubLink: "https://github.com/SandraSor/Counter_react",
-  },
-  {
-    id: 2,
-    title: "Quiz",
-    skills: "Skills: HTML, CSS, React",
-    img: project02,
-    imgBig: project02Big,
-    gitHubLink: "https://github.com/SandraSor/Quiz_react",
-  },
-  {
-    id: 3,
-    title: "User List",
-    skills: "Skills: HTML, CSS, React",
+	{
+		id: 1,
+		title: 'Counter',
+		skills: 'Skills: HTML, CSS, React',
+		img: project01,
+		imgBig: project01Big,
+		gitHubLink: 'https://github.com/SandraSor/Counter_react',
+	},
+	{
+		id: 2,
+		title: 'Quiz',
+		skills: 'Skills: HTML, CSS, React',
+		img: project02,
+		imgBig: project02Big,
+		gitHubLink: 'https://github.com/SandraSor/Quiz_react',
+	},
+	{
+		id: 3,
+		title: 'User List',
+		skills: 'Skills: HTML, CSS, React',
 
-    img: project03,
-    imgBig: project03Big,
-    gitHubLink: "https://github.com/SandraSor/UserList_react",
-  },
-  {
-    id: 4,
-    skills: "Skills: HTML, CSS, React",
-    title: "Tic-Tac-Toe",
-    img: project04,
-    imgBig: project04Big,
-    gitHubLink: "https://github.com/SandraSor/XO-my-app",
-  },
-  {
-    id: 5,
-    skills: "Skills: HTML, CSS, React",
-    title: "Sneakers",
-    img: project05,
-    imgBig: project05Big,
-    gitHubLink: "https://github.com/SandraSor/",
-  },
+		img: project03,
+		imgBig: project03Big,
+		gitHubLink: 'https://github.com/SandraSor/UserList_react',
+	},
+	{
+		id: 4,
+		skills: 'Skills: HTML, CSS, React',
+		title: 'Tic-Tac-Toe',
+		img: project04,
+		imgBig: project04Big,
+		gitHubLink: 'https://github.com/SandraSor/XO-my-app',
+	},
+	{
+		id: 5,
+		skills: 'Skills: HTML, CSS, React',
+		title: 'Sneakers',
+		img: project05,
+		imgBig: project05Big,
+		gitHubLink: 'https://github.com/SandraSor/sneakers-react',
+	},
 ];
 
 export { projects };

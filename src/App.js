d64b91768063c80f7e248017e4c9ac90d2cd3c09
SandraSor <soronastya@mail.ru>
2023-03-18 13:44:19 +0300

    navbar sticky to top

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import "./styles/main.css";
+import './styles/main.css';
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from "./components/navbar/Navbar";
-import Footer from "./components/footer/Footer";
-import Home from "./pages/Home";
-import Contacts from "./pages/Contacts";
-import Skills from "./pages/Skills";
-import Project from "./pages/Project";
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home';
+import Contacts from './pages/Contacts';
+import Skills from './pages/Skills';
+import Project from './pages/Project';
 
-import ScrollToTop from "./utils/scrollToTop";
+import ScrollToTop from './utils/scrollToTop';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <ScrollToTop />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/project/:id" element={<Project />}></Route>
-          <Route path="/skills" element={<Skills />}></Route>
-          <Route path="/contacts" element={<Contacts />}></Route>
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router>
+				<ScrollToTop />
+				<Navbar />
+				<Routes>
+					<Route path='/Portfolio-with-React' element={<Home />}></Route>
+					<Route path='/project/:id' element={<Project />}></Route>
+					<Route path='/skills' element={<Skills />}></Route>
+					<Route path='/contacts' element={<Contacts />}></Route>
+				</Routes>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,57 +1,57 @@
-import { NavLink } from "react-router-dom";
-import BtnDarkMode from "../buttonDarkMode/BtnDarkMode";
-import "./styles.css";
+import { NavLink } from 'react-router-dom';
+import BtnDarkMode from '../buttonDarkMode/BtnDarkMode';
+import './styles.css';
 
 const Navbar = () => {
-  const activeLink = "nav-list__link nav-list__link--active";
-  const normalLink = "nav-list__link";
+	const activeLink = 'nav-list__link nav-list__link--active';
+	const normalLink = 'nav-list__link';
 
-  return (
-    <nav className="nav">
-      <div className="container">
-        <div className="nav-row">
-          <NavLink to="/" className="logo">
-            <strong>Freelancer</strong> portfolio
-          </NavLink>
+	return (
+		<nav className='nav'>
+			<div className='container'>
+				<div className='nav-row'>
+					<NavLink to='/Portfolio-with-React' className='logo'>
+						<strong>Freelancer</strong> portfolio
+					</NavLink>
 
-          <BtnDarkMode />
+					<BtnDarkMode />
 
-          <ul className="nav-list">
-            <li className="nav-list__item">
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive ? activeLink : normalLink
-                }
-              >
-                Projects
-              </NavLink>
-            </li>
-            <li className="nav-list__item">
-              <NavLink
-                to="/skills"
-                className={({ isActive }) =>
-                  isActive ? activeLink : normalLink
-                }
-              >
-                Skills
-              </NavLink>
-            </li>
-            <li className="nav-list__item">
-              <NavLink
-                to="/contacts"
-                className={({ isActive }) =>
-                  isActive ? activeLink : normalLink
-                }
-              >
-                Contacts
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+					<ul className='nav-list'>
+						<li className='nav-list__item'>
+							<NavLink
+								to='/Portfolio-with-React'
+								className={({ isActive }) =>
+									isActive ? activeLink : normalLink
+								}
+							>
+								Projects
+							</NavLink>
+						</li>
+						<li className='nav-list__item'>
+							<NavLink
+								to='/skills'
+								className={({ isActive }) =>
+									isActive ? activeLink : normalLink
+								}
+							>
+								Skills
+							</NavLink>
+						</li>
+						<li className='nav-list__item'>
+							<NavLink
+								to='/contacts'
+								className={({ isActive }) =>
+									isActive ? activeLink : normalLink
+								}
+							>
+								Contacts
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
